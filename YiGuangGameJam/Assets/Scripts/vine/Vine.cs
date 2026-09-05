dfc229a4fb62691a6e806fb736fcf0b7022d8dd5
3460797530 <3460797530@qq.com>
2026-09-05 16:26:14 +0800

    6.平滑转向

--- YiGuangGameJam/Assets/Scripts/vine/Vine.cs
+++ YiGuangGameJam/Assets/Scripts/vine/Vine.cs
@@ -7,6 +7,7 @@ public class Vine : MonoBehaviour
     [SerializeField] private float growSpeed = 3f; // 藤曼生长速度，即藤曼头移动速度
     [SerializeField] private float pointDistance = 0.1f; // 记录的两个藤曼点之间的距离
     [SerializeField] private float maxTurnAngle = 60; // 最大转向角
+    [SerializeField] private float turnSpeed = 90 * Mathf.Deg2Rad; // 每秒可转过的最大弧度，即转向速度
 
     private Transform vineHead; // 藤曼头
     private Vector3 growDirection = Vector2.up; // 藤曼生长方向
@@ -14,6 +15,7 @@ public class Vine : MonoBehaviour
     private List<Vector3> pointList = new List<Vector3>(); // 藤曼点位置列表
     private Vector3 lastPoint; // 记录的上一个藤蔓点的位置
     private LineRenderer lineRenderer; // 用来连点成线的
+    private Vector3 targetDirection; // 藤曼经过限制后的目标方向
 
  
     // Start is called before the first frame update
@@ -36,8 +38,7 @@ public class Vine : MonoBehaviour
     // 藤曼生长
     private void VineGrow()
     {
-        if (Input.GetMouseButtonDown(0))
-            UpdateGrowDirection();
+        UpdateGrowDirection();
 
         VineHeadMove();
 
@@ -67,12 +68,19 @@ public class Vine : MonoBehaviour
 
     // 通过鼠标更新藤曼生长方向
     private void UpdateGrowDirection()
-    {
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // 获取鼠标世界坐标
-        mouseWorldPos.z = vineHead.position.z; // 锁定鼠标世界坐标的z轴
-        Vector3 targetDirection = (mouseWorldPos - vineHead.position).normalized; // 更新并单位化目标方向
-        float angle = Vector2.SignedAngle(initialDirection, targetDirection); // 计算目标方向与初始方向之间夹角
-        angle = Mathf.Clamp(angle,-maxTurnAngle, maxTurnAngle); // 若夹角数值大于限制的最大角度，将其值修改为限制的最大角度
-        growDirection = Quaternion.AngleAxis(angle, Vector3.forward) * initialDirection; // 改变生长方向
+    {   // 按下鼠标左键再计算目标方向
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // 获取鼠标世界坐标
+            mouseWorldPos.z = vineHead.position.z; // 锁定鼠标世界坐标的z轴
+
+            Vector3 Direction = (mouseWorldPos - vineHead.position).normalized; // 更新并单位化目标方向
+            float angle = Vector2.SignedAngle(initialDirection, Direction); // 计算目标方向与初始方向之间夹角
+            angle = Mathf.Clamp(angle, -maxTurnAngle, maxTurnAngle); // 若夹角数值大于限制的最大角度，将其值修改为限制的最大角度
+
+            targetDirection = Quaternion.AngleAxis(angle, Vector3.forward) * initialDirection; // 确定经过限制的目标方向
+        }
+
+        growDirection = Vector3.RotateTowards(growDirection, targetDirection, turnSpeed * Time.deltaTime, 0); // 藤曼生长方向逐渐靠向目标方向，每帧计算
     }
 }
