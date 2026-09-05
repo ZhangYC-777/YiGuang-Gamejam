bb49f42ce2721b10c7fc57d206d2358d577cfbc7
3460797530 <3460797530@qq.com>
2026-09-05 13:40:43 +0800

    切换藤曼生长方式第一步：实现藤曼头的移动

---
+++ YiGuangGameJam/Assets/Scripts/vine/Vine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vine : MonoBehaviour
+{
+    [SerializeField] private float growSpeed = 3f; // 藤曼生长速度，即tomato移动速度
+    [SerializeField] private Vector2 growDirection = Vector2.up; // 藤曼生长方向
+
+    [SerializeField] private GameObject vineHead; // tomato
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vineHead = GameObject.Find("Head");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vineHead.transform.position += (Vector3)(growSpeed * growDirection * Time.deltaTime);
+    }
+}
