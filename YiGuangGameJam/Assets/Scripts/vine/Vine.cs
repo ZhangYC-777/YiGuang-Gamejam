7d8ed6900de8c78a4937a430d0965b67663e72c2
3460797530 <3460797530@qq.com>
2026-09-05 14:52:11 +0800

    第三步：利用藤曼头位置连点成线绘制藤曼

--- YiGuangGameJam/Assets/Scripts/vine/Vine.cs
+++ YiGuangGameJam/Assets/Scripts/vine/Vine.cs
@@ -6,30 +6,49 @@ public class Vine : MonoBehaviour
 {
     [SerializeField] private float growSpeed = 3f; // 藤曼生长速度，即藤曼头移动速度
     [SerializeField] private Vector2 growDirection = Vector2.up; // 藤曼生长方向
-    [SerializeField] private float pointDistance = 1.0f; // 记录的两个藤曼点之间的距离
+    [SerializeField] private float pointDistance = 0.1f; // 记录的两个藤曼点之间的距离
     [SerializeField] private List<Vector3> pointList; // 藤曼点位置列表
 
     private Transform vineHead; // 藤曼头
     private Vector3 lastPoint; // 记录的上一个藤蔓点的位置
+    private LineRenderer lineRenderer; // 用来连点成线的
  
     // Start is called before the first frame update
     void Start()
     {
         vineHead = GameObject.Find("Head").GetComponent<Transform>();
-        pointList.Add(vineHead.position);
-        lastPoint = vineHead.position;
+        lineRenderer = GetComponent<LineRenderer>();
+
+        DrawLine();
     }
 
     // Update is called once per frame
     void Update()
     {
+        VineGrow();
+    }
+
+    // 藤曼生长
+    private void VineGrow()
+    {
         vineHead.transform.position += (Vector3)(growSpeed * growDirection * Time.deltaTime);
 
         if (Vector3.Distance(vineHead.position, lastPoint) > pointDistance)
         {
-            pointList.Add(vineHead.position);
-            lastPoint = vineHead.position;
-            Debug.Log("aaa");
+            DrawLine();
+        }
+    }
+
+    // 绘制藤曼
+    private void DrawLine()
+    {
+        pointList.Add(vineHead.position);
+        lastPoint = vineHead.position;
+
+        lineRenderer.positionCount = pointList.Count;
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            lineRenderer.SetPosition(i, pointList[i]);
         }
     }
 }
