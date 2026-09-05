d4b705b3d27a8acfd31e39bb76d0267731850f0b
3460797530 <3460797530@qq.com>
2026-09-05 14:03:55 +0800

    第二步：记录藤曼头位置

--- YiGuangGameJam/Assets/Scripts/vine/Vine.cs
+++ YiGuangGameJam/Assets/Scripts/vine/Vine.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Vine : MonoBehaviour
 {
-    [SerializeField] private float growSpeed = 3f; // 藤曼生长速度，即tomato移动速度
+    [SerializeField] private float growSpeed = 3f; // 藤曼生长速度，即藤曼头移动速度
     [SerializeField] private Vector2 growDirection = Vector2.up; // 藤曼生长方向
+    [SerializeField] private float pointDistance = 1.0f; // 记录的两个藤曼点之间的距离
+    [SerializeField] private List<Vector3> pointList; // 藤曼点位置列表
 
-    [SerializeField] private GameObject vineHead; // tomato
-
+    private Transform vineHead; // 藤曼头
+    private Vector3 lastPoint; // 记录的上一个藤蔓点的位置
+ 
     // Start is called before the first frame update
     void Start()
     {
-        vineHead = GameObject.Find("Head");
+        vineHead = GameObject.Find("Head").GetComponent<Transform>();
+        pointList.Add(vineHead.position);
+        lastPoint = vineHead.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         vineHead.transform.position += (Vector3)(growSpeed * growDirection * Time.deltaTime);
+
+        if (Vector3.Distance(vineHead.position, lastPoint) > pointDistance)
+        {
+            pointList.Add(vineHead.position);
+            lastPoint = vineHead.position;
+            Debug.Log("aaa");
+        }
     }
 }
